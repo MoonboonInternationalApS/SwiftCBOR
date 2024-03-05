58d60d56c7ef294531e460c00dbf72bb04ed56c7
Vasily Polyuhovich <vasyl@adaalarm.com>
2024-03-05 06:51:17 +0200

    Remove commented code

--- Sources/Util.swift
+++ Sources/Util.swift
@@ -1,24 +1,8 @@
 final class Util {
     // https://stackoverflow.com/questions/24465475/how-can-i-create-a-string-from-utf8-in-swift
     static func decodeUtf8(_ bytes: ArraySlice<UInt8>) throws -> String {
+        guard !bytes.isEmpty else { return "" }
         return String(decoding: bytes, as: UTF8.self)
-        
-//        var result = ""
-//        var decoder = UTF8()
-//        var generator = bytes.makeIterator()
-//        var finished = false
-//        repeat {
-//            let decodingResult = decoder.decode(&generator)
-//            switch decodingResult {
-//            case .scalarValue(let char):
-//                result.append(String(char))
-//            case .emptyInput:
-//                finished = true
-//            case .error:
-//                throw CBORError.incorrectUTF8String
-//            }
-//        } while (!finished)
-//        return result
     }
 
     static func djb2Hash(_ array: [Int]) -> Int {
